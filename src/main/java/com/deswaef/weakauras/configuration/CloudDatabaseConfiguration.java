72985e54c1709fcd66d4cef0f39a6ce10a552fab
Quinten De Swaef <quintendeswaef@gmail.com>
2015-03-12 00:22:38 +0100

    changing name to productiondb

--- src/main/java/com/deswaef/weakauras/configuration/CloudDatabaseConfiguration.java
+++ src/main/java/com/deswaef/weakauras/configuration/CloudDatabaseConfiguration.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@Profile({"cloud"})
+@Profile({"production"})
 @PropertySource(value = "classpath:cloudpersistence.properties")
 public class CloudDatabaseConfiguration {
 
