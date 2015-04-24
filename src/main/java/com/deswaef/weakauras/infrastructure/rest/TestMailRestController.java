9df3c5da593e1b7fc6d4e836c66ff7d44addce00
Quinten De Swaef <quinten.deswaef@ipc.be>
2015-04-24 15:14:08 +0200

    Create a controller to test emails as admin

---
+++ src/main/java/com/deswaef/weakauras/infrastructure/rest/TestMailRestController.java
@@ -0,0 +1,58 @@
+package com.deswaef.weakauras.infrastructure.rest;
+
+import com.deswaef.weakauras.infrastructure.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/admin/emails")
+public class TestMailRestController {
+
+    @Autowired
+    private MailService mailService;
+
+    @Value("${com.deswaef.scrappie.fullbaseurl}")
+    private String fullBaseUrl;
+
+    @RequestMapping(value = "/test")
+    public HttpEntity<String> sendEmail() {
+        try {
+            mailService
+                    .createMail()
+                    .htmlBody(readMarshalledFile("/emails/testemail.html").replace("${com.deswaef.scrappie.fullbaseur}", fullBaseUrl))
+                    .to("quintendeswaef@gmail.com")
+                    .subject("WowScrappie - Activate your Registration")
+                    .send();
+            return new HttpEntity<>("email was sent");
+        } catch (Exception ex) {
+            return new HttpEntity<>("Failure when sending: " + ex.getMessage());
+        }
+    }
+
+    public static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+    }
+
+
+    public String readMarshalledFile(String s) throws IOException {
+        URL url = this.getClass().getResource(s);
+        File htmlFile = new File(url.getFile());
+        return readFile(URLDecoder.decode(htmlFile.getPath()), Charset.defaultCharset());
+    }
+
+}
