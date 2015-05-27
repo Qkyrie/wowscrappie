bc7a150020009781aca5fc3ed8e2b85597b38082
Quinten De Swaef <quintendeswaef@gmail.com>
2015-05-27 21:03:24 +0200

    Manage server

---
+++ src/main/resources/static/assets/js/custom/admin/manage-server.js
@@ -0,0 +1,24 @@
+var ManageServerVM  = function() {
+
+    var self = this;
+    self.overallHealth = ko.observable('');
+    self.diskspaceHealth = ko.observable('');
+    self.redisHealth = ko.observable('');
+    self.dbHealth = ko.observable('');
+
+    self.getHealth = function() {
+
+        $.get('/manage/health', function(data) {
+
+            self.overallHealth(data.status);
+            self.diskspaceHealth(data.diskSpace);
+            self.redisHealth(data.redis);
+            self.dbHealth(data.db);
+        });
+    };
+
+    self.getHealth();
+};
+
+var manageServerVM = new ManageServerVM();
+ko.applyBindings(manageServerVM, $("#page-inner")[0]);
