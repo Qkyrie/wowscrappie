0050062e055c83fca5ae45f994e9a4a58e4b7eed
Quinten De Swaef <quintendeswaef@gmail.com>
2016-02-12 13:02:27 +0100

    operatorwrap checkstyle

--- src/main/java/com/deswaef/wowscrappie/usermanagement/controller/LoginController.java
+++ src/main/java/com/deswaef/wowscrappie/usermanagement/controller/LoginController.java
@@ -13,9 +13,9 @@ public class LoginController {
     @RequestMapping(method = RequestMethod.GET)
     public String login() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null &&
-                authentication.isAuthenticated() &&
-                !authentication.getPrincipal().equals("anonymousUser")) {
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !authentication.getPrincipal().equals("anonymousUser")) {
             return "redirect:/";
         } else {
             return "login";
