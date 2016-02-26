fc00c16c2f4b196069ebf8192f9b06cd7d689f05
Quinten De Swaef <quintendeswaef@gmail.com>
2016-02-26 22:05:29 +0100

    Not historic anymore, we're deleting old snapshots in favor of the newer ones

--- core/src/main/java/com/deswaef/wowscrappie/auctionhouse/continuous/OldAuctionHouseSnapshotsDeleter.java
+++ core/src/main/java/com/deswaef/wowscrappie/auctionhouse/continuous/OldAuctionHouseSnapshotsDeleter.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HistoricAuctionsExporterScheduler {
+public class OldAuctionHouseSnapshotsDeleter {
 
     @Autowired
     private AuctionsExporter auctionsExporter;
