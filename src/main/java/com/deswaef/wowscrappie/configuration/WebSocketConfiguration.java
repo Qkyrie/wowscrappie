02f969c3e3b68260f19fd15701da398b84c9f888
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-08 22:38:44 +0100

    new version of sockjs

--- src/main/java/com/deswaef/wowscrappie/configuration/WebSocketConfiguration.java
+++ src/main/java/com/deswaef/wowscrappie/configuration/WebSocketConfiguration.java
@@ -5,15 +5,17 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.ExpiringSession;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableScheduling
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
 
-    protected void configureStompEndpoints(StompEndpointRegistry registry) { // <2>
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/messages")
                 .setAllowedOrigins("http://localhost:8090",
                         "http://localhost:8080",
@@ -30,6 +32,7 @@ public class WebSocketConfiguration extends AbstractSessionWebSocketMessageBroke
                 .withSockJS();
     }
 
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/queue/", "/topic/");
         registry.setApplicationDestinationPrefixes("/app");
