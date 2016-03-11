596eb3f29c6b8819c0caccc1a17757f2b046ff24
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-11 12:41:44 +0100

    we're using spring session, so we can use the spring-session-websocket support

--- wowscrappie-web/src/main/java/com/deswaef/wowscrappie/configuration/WebSocketConfiguration.java
+++ wowscrappie-web/src/main/java/com/deswaef/wowscrappie/configuration/WebSocketConfiguration.java
@@ -3,6 +3,8 @@ package com.deswaef.wowscrappie.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,10 +12,10 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableScheduling
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfiguration extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void configureStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/messages")
                 .setAllowedOrigins("http://localhost:8090",
                         "http://localhost:8080",
