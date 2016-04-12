b20089c07bd8ea9cbc7ddfac40a263bebf7f4f54
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-12 23:20:51 +0200

    removal of typings folder

--- wowscrappie-web/src/main/resources/static/assets/typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="main/ambient/c3/index.d.ts" />
-/// <reference path="main/ambient/d3/index.d.ts" />
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
-/// <reference path="main/ambient/typeahead/index.d.ts" />
