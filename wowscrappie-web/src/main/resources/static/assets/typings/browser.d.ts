7efb0d931e62f9e8b6f79a8b315604ccff14cf1a
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-12 23:08:41 +0200

    typings are persistent now

--- wowscrappie-web/src/main/resources/static/assets/typings/browser.d.ts
+++ wowscrappie-web/src/main/resources/static/assets/typings/browser.d.ts
@@ -1,3 +1,6 @@
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/c3/index.d.ts" />
+/// <reference path="browser/ambient/d3/index.d.ts" />
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/typeahead/index.d.ts" />
