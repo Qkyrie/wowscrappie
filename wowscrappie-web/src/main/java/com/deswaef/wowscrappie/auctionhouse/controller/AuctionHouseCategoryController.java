0946208961a14fe255a80499dc3c70ea2c5e3681
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-15 21:42:04 +0200

    created controller for subcategories

--- wowscrappie-web/src/main/java/com/deswaef/wowscrappie/auctionhouse/controller/AuctionHouseCategoryController.java
+++ wowscrappie-web/src/main/java/com/deswaef/wowscrappie/auctionhouse/controller/AuctionHouseCategoryController.java
@@ -1,4 +1,33 @@
 package com.deswaef.wowscrappie.auctionhouse.controller;
 
+import com.deswaef.wowscrappie.auctionhouse.domain.AuctionHouseSubCategory;
+import com.deswaef.wowscrappie.auctionhouse.service.AuctionHouseCategoryService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "/auctionhouse/subcategory")
 public class AuctionHouseCategoryController {
+
+    @Autowired
+    private AuctionHouseCategoryService auctionHouseCategoryService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{subcategoryId}")
+    public String bySubcategory(ModelMap modelMap, @PathVariable("subcategoryId") Long subcategoryId) {
+        Optional<AuctionHouseSubCategory> auctionHouseSubCategory = auctionHouseCategoryService.subCategoryById(subcategoryId);
+        if (auctionHouseSubCategory.isPresent()) {
+            modelMap.put("subcategory", auctionHouseSubCategory.get());
+            return "auctionhouse/subcategory/subcategory";
+        } else {
+            return "auctionhouse/subcategory/not-found";
+        }
+    }
+
 }
