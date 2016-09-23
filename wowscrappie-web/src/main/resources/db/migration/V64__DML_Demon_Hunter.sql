3e6bf90895ecd997885733b13210adbdf635b9b0
Quinten De Swaef <quintendeswaef@gmail.com>
2016-09-23 15:24:01 +0200

    Adding demon hunter

---
+++ wowscrappie-web/src/main/resources/db/migration/V64__DML_Demon_Hunter.sql
@@ -0,0 +1,4 @@
+insert into wowclass(id, name, slug) values (12, 'Demon Hunter', 'demon_hunter');
+insert into wowspec(id, name, slug, wowclass_id) values (35, 'Arms', 'arms', 12);
+insert into wowspec(id, name, slug, wowclass_id) values (36, 'Fury', 'fury', 12);
+insert into wowspec(id, name, slug, wowclass_id) values (37, 'Protection', 'protection', 12);
