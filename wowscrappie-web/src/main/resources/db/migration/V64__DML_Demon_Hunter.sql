e4229c2cdbf68684308dea459b2a93978be6c812
Quinten De Swaef <quintendeswaef@gmail.com>
2016-11-26 17:53:07 +0100

    removing migration

--- wowscrappie-web/src/main/resources/db/migration/V64__DML_Demon_Hunter.sql
+++
@@ -1,4 +0,0 @@
-insert into wowclass(id, name, slug) values (13, 'Demon Hunter', 'demon_hunter');
-insert into wowspec(id, name, slug, wowclass_id) values (35, 'Arms', 'arms', 13);
-insert into wowspec(id, name, slug, wowclass_id) values (36, 'Fury', 'fury', 13);
-insert into wowspec(id, name, slug, wowclass_id) values (37, 'Protection', 'protection', 13);
