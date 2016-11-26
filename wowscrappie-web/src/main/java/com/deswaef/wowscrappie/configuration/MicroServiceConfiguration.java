dcf8ce84aae67b620a27f79a432892c7e843bfa2
Quinten De Swaef <quintendeswaef@gmail.com>
2016-11-26 17:10:29 +0100

    removal of useless microservices

--- wowscrappie-web/src/main/java/com/deswaef/wowscrappie/configuration/MicroServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.deswaef.wowscrappie.configuration;
-
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableEurekaClient
-public class MicroServiceConfiguration {
-}
