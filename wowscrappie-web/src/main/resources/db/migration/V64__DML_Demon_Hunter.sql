c7838763c6a2b8b2e51d3c392725dea018c01ffd
Quinten De Swaef <quintendeswaef@gmail.com>
2016-11-26 17:35:27 +0100

    bugfix to changelogs

--- wowscrappie-web/src/main/resources/db/migration/V64__DML_Demon_Hunter.sql
+++ wowscrappie-web/src/main/resources/db/migration/V64__DML_Demon_Hunter.sql
@@ -1,4 +1,4 @@
-insert into wowclass(id, name, slug) values (12, 'Demon Hunter', 'demon_hunter');
-insert into wowspec(id, name, slug, wowclass_id) values (35, 'Arms', 'arms', 12);
-insert into wowspec(id, name, slug, wowclass_id) values (36, 'Fury', 'fury', 12);
-insert into wowspec(id, name, slug, wowclass_id) values (37, 'Protection', 'protection', 12);
+insert into wowclass(id, name, slug) values (13, 'Demon Hunter', 'demon_hunter');
+insert into wowspec(id, name, slug, wowclass_id) values (35, 'Arms', 'arms', 13);
+insert into wowspec(id, name, slug, wowclass_id) values (36, 'Fury', 'fury', 13);
+insert into wowspec(id, name, slug, wowclass_id) values (37, 'Protection', 'protection', 13);
