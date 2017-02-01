1c87b7385c0e2610258ab725c34d26e8bcb76b29
Quinten De Swaef <quintendeswaef@gmail.com>
2017-02-01 14:01:14 +0100

    demon hunter added'

---
+++ wowscrappie-web/src/main/resources/db/migration/V64__Demon_Hunter.sql
@@ -0,0 +1,1 @@
+insert into wowclass(id, name, slug) values(13, 'Demon Hunter', 'demon_hunter');
---
+++ wowscrappie-web/src/main/resources/db/migration/V65__demon_hunter_specs.sql
@@ -0,0 +1,7 @@
+insert into wowspec(id, name, slug, wowclass_id) values(
+  39, 'Havoc', 'havoc', 13
+);
+
+insert into wowspec(id, name, slug, wowclass_id) values(
+  40, 'Vengeance', 'vengeance', 13
+);
